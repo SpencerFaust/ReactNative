498bd5e20ad985e3ecfacfeff6e38786c5a742a9
Spencer Faust <spencer.faust@tempworks.com>
2019-05-21 12:11:32 -0500

    React Native 'Hello World!'

--- App.js
+++ App.js
@@ -1,1 +1,12 @@
-console.log("Hello World.");
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+export default class HelloWorldApp extends Component {
+    render() {
+        return (
+            <View style={{ flex: 1, justifyContent: "center", alignItems: 'center' }}>
+                <Text>Hello World!</Text>
+            </View>
+        )
+    }
+}
